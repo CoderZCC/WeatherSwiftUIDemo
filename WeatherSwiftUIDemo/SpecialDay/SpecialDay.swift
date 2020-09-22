7505c19de7ab059e7f5eaf1078e7c1eda283b258
zcc <coderzcca@gmail.com>
2020-09-22 13:12:57 +0800

    no message

---
+++ WeatherSwiftUIDemo/SpecialDay/SpecialDay.swift
@@ -0,0 +1,98 @@
+//
+//  SpecialDay.swift
+//  SpecialDay
+//
+//  Created by 北京摩学教育科技有限公司 on 2020/9/22.
+//  Copyright © 2020 zcc. All rights reserved.
+//
+
+import WidgetKit
+import SwiftUI
+
+/// [0, x)
+let kRandomInt = Int(arc4random()) % 5
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), imageIndex: kRandomInt)
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), imageIndex: kRandomInt)
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        /// 随机数
+        let timeline = Timeline(entries: [SimpleEntry(date: Date(), imageIndex: kRandomInt)], policy: .atEnd)
+        
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let imageIndex: Int
+    
+    let _calendar = Calendar.current
+    var _specilDay: Date {
+        let day = "2018-01-27"
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone.current
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.date(from: day) ?? Date()
+    }
+    
+    var year: Int {
+        return self._calendar.dateComponents([.year], from: self._specilDay, to: self.date).year ?? 0
+    }
+    var day: Int {
+        return self._calendar.dateComponents([.day], from: self._specilDay, to: self.date).day ?? 0
+    }
+    var dateFormatter: String {
+        let hour = self._calendar.dateComponents([.hour], from: self._specilDay, to: self.date).hour ?? 0
+        return "\(hour)小时"
+    }
+}
+
+struct SpecialDayEntryView : View {
+    var entry: Provider.Entry
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .center, vertical: .center)) {
+            Image("love\(entry.imageIndex)").resizable().aspectRatio(contentMode: .fill)
+            VStack {
+                Text("我们在一起已经").font(.system(size: 14.0, weight: .semibold)).modifier(ContentTextModifier())
+                Text("\(entry.day)").font(.custom("DINCond-Bold", size: 60.0)).modifier(ContentTextModifier()).padding(4.0)
+                Text(entry.dateFormatter).font(.system(size: 12.0, weight: .bold)).modifier(ContentTextModifier())
+            }
+        }.foregroundColor(.white)
+    }
+}
+
+// MARK: -文字内容样式
+struct ContentTextModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content.shadow(color: .init(UIColor.black.withAlphaComponent(0.8)), radius: 10, x: 0.0, y: 0.0)
+    }
+}
+
+@main
+struct SpecialDay: Widget {
+    let kind: String = "SpecialDay"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            SpecialDayEntryView(entry: entry)
+        }
+        .configurationDisplayName("纪念日")
+        .description("记录在一起的日子")
+    }
+}
+
+struct SpecialDay_Previews: PreviewProvider {
+    static var previews: some View {
+        SpecialDayEntryView(entry: SimpleEntry(date: Date(), imageIndex: kRandomInt))
+            .previewContext(WidgetPreviewContext(family: .systemSmall))
+    }
+}
